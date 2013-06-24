e5592a757d0eb348c64a349adbc7c6bb1af83e56
Fábio Pinheiro <FabioPinheiro@ASUS-X54C>
2013-06-24 22:31:28 +0100

    MainMenu heading

--- bob-running-puzzle/src/com/epic/bobrunningpuzzle/sceens/MainMenu.java
+++ bob-running-puzzle/src/com/epic/bobrunningpuzzle/sceens/MainMenu.java
@@ -2,15 +2,20 @@ package com.epic.bobrunningpuzzle.sceens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.epic.bobrunningpuzzle.BobRunningPuzzle;
 
 public class MainMenu implements Screen {
 
@@ -49,10 +54,12 @@ public class MainMenu implements Screen {
 		
 		table = new Table(skin);
 		table.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-
+		
+		//Creating Fonts
 		whiteFont = new BitmapFont(Gdx.files.internal("font/whiteFont.fnt"),false);
 		blackFont = new BitmapFont(Gdx.files.internal("font/blackFont.fnt"),false);
 		
+		//Creating Buttons
 		TextButtonStyle textButtonStyle = new TextButtonStyle();
 		textButtonStyle.up = skin.getDrawable("button.up");
 		textButtonStyle.down = skin.getDrawable("button.down");
@@ -61,8 +68,23 @@ public class MainMenu implements Screen {
 		textButtonStyle.font = blackFont;
 		
 		buttonExit = new TextButton("EXIT", textButtonStyle);
+		buttonExit.addListener(new ClickListener(){
+			@Override
+			public void clicked(InputEvent event, float x, float y){
+				Gdx.app.exit();
+			}
+		});
 		buttonExit.pad(20);
 		
+		//Creating heading
+		LabelStyle headingStyle = new LabelStyle(whiteFont, Color.WHITE);
+		
+		heading = new Label(BobRunningPuzzle.TITLE, headingStyle);
+		//heading.setFontScale(1.5f);
+		
+		//Puting stuff together
+		table.add(heading);
+		table.row();
 		table.add(buttonExit);
 		table.debug(); //TODO remove later
 		stage.addActor(table);
